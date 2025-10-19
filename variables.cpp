ab3a5f1c34b036c1c08576492cc27febbf825a5c
Manoj 020 <manojreddy0208@gmail.com>
2025-10-18 20:09:04 -0500

    This are variables in C++

---
+++ variables.cpp
@@ -0,0 +1,23 @@
+# include<iostream>
+int main()
+{
+    int x = 5;
+    int y = 4;
+    int sum = x+y;
+    std::cout<<"The Sum of x and y is "<<sum;
+    //Double
+    double PI = 3.14;
+    double Price = 5.99;
+
+    //Char
+    char first_letter = 'A'; // this is a single character
+    //Boolean
+    bool is_studying = false;
+    bool is_awake = true;
+
+    //string this are set of characters
+
+    std::string carnumber = "FE621";
+    std::string first_name = "MJ";
+    return 0;
+}
