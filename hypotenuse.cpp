2394a9a0caea8f7d2e53ceee26d916d72d10890f
Manoj 020 <manojreddy0208@gmail.com>
2025-10-22 17:30:05 -0500

    some simple program in c++ which calculates hypotenuse

---
+++ hypotenuse.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cmath>
+
+int main(){
+    double a;
+    double b;
+    double c;
+    std::cout<<"Enter Side A: ";
+    std::cin>>a;
+    std::cout<<"Enter Side B: ";
+    std::cin>>b;
+    a = pow(a,2);
+    b = pow(b,2);
+    c = sqrt(a+b);
+    std::cout<<"Hypotenuse is "<<c;
+    return 0;
+}
--- math.cpp
+++ math.cpp
@@ -1,6 +1,19 @@
 #include<iostream>
+#include<cmath>
 
 int main(){
-
+    //useful math functions
+    double x = 3.78;
+    double y = 4;
+    double z;
+    // z = std::max(x,y); this gives us the max of two values
+    // z = std::min(x,y); this gives us the min of two values
+    // z = pow(2,2); we have power the first number is just a number and the second one how much you want to raise the power
+    // z = sqrt(16); we have sqrt to get the sqrt of the number
+    // z = round(x); // just rounds to nearest integer
+    // z = abs(-19); this give us the positive number
+    // z = ceil(x);
+    // z = floor(x); floor just reduces the number
+    std::cout<<z;
     return 0;
 }
