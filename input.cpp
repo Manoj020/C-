55c8c197ad16dd095f3e5f2507be8efe08d782af
Manoj 020 <manojreddy0208@gmail.com>
2025-10-22 17:04:30 -0500

    input's can be tricky we have cin and getline for get line make sure to use the std::ws

---
+++ input.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+int main(){
+    // how c in is used to get user input we getline function 
+    // cout << (insertion operator)
+    // cin >> (extraction operator)
+    std::string name; // variable name
+    int age;
+    std::cout<<"Enter your Name:";
+    std::getline(std::cin>>std::ws, name);// why ws (white space in the input buffer when we move lines from one place to another )
+    std::cout<<"Enter your age:";
+    std::cin>>age;
+    std::cout << "Hello "<<name<<std::endl;
+    std::cout<<"You are "<<age <<" Years old";
+    return 0;
+}
