83df8d89a530c7408608f91d8b1c52172828f7b0
Manoj 020 <manojreddy0208@gmail.com>
2025-10-22 19:53:51 -0500

    Simple Calculator program using c++ switch

---
+++ Conditions/calculator.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "******* CALCULATOR ******" << std::endl;
+    char op;
+    double num1, num2, result;
+    bool is_valid = true;
+    std::cout << "Enter the Operation: ";
+    std::cin >> op;
+    std::cout << "Enter Number1: ";
+    std::cin >> num1;
+    std::cout << "Enter Number2: ";
+    std::cin >> num2;
+    switch (op)
+    {
+    case '+':
+        result = num1 + num2;
+        break;
+    case '-':
+        result = num1 - num2;
+        break;
+    case '*':
+        result = num1 * num2;
+        break;
+    case '/':
+        if (num2 == 0)
+        {
+            std::cout << "Can't Divide by zero" << std::endl;
+            is_valid = false;
+        }
+        else
+        {
+            result = num1 / num2;
+        }
+        break;
+    default:
+        std::cout << "Invalid Operator";
+        break;
+    }
+    if (is_valid)
+    {
+        std::cout << "Result " << result << std::endl;
+    }
+    std::cout << "*******************" << std::endl;
+    return 0;
+}
