a8ac3e0c52704930d4ae2ca334f7a367c130b414
Manoj 020 <manojreddy0208@gmail.com>
2025-10-30 17:19:53 -0500

    Some useful string functions are length,empty,clear,append,at,insert,find,erase

---
+++ Conditions/string-method.cpp
@@ -0,0 +1,25 @@
+
+#include<iostream>
+
+int main(){
+    std::string name;
+    std::cout<<"Enter Your Name: ";
+    std::getline(std::cin,name);
+    // std::cout<<"The length of your name is "<<name.length()<<std::endl;
+    // if(name.empty()){
+    //     std::cout<<"Your Name Cannot be empty";
+    // }
+    // else{
+    //     std::cout<<name;
+    // }
+    // name.clear();
+    // std::cout<<name;
+    // name = name.append("0208");
+    // std::cout<<name;
+    // std::cout<<name.at(0);
+    // name.insert(0,"Hello ");
+    // std::cout<<name;
+    //we also have find and erase too
+    return 0;
+
+}
