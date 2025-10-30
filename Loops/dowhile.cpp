4c2144557c9b3da4efac71c6ca26e3af6c899c29
Manoj 020 <manojreddy0208@gmail.com>
2025-10-30 18:03:20 -0500

    Do while loops in C++ do a condtion first and after that repeat until it is true

---
+++ Loops/dowhile.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+int main(){
+    //do while loop exceute the condition once and check the condition again until it is true
+    int number;
+    do{
+        std::cout<<"Enter a number: ";
+        std::cin>>number;
+        std::cout<<number; // do this first after until the condition is met
+    }while(number<0);
+    return 0;
+}
