3d85e52a321e63cd47540998675e3bd6841e41ca
Manoj 020 <manojreddy0208@gmail.com>
2025-11-04 10:46:41 -0600

    Arrays in C++

---
+++ Arrays/array.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+int main(){
+    std::string names[4];
+    names[0] = "Manoj";
+    names[1] = "Jag";
+    names[2] = "SK";
+    names[3] = "Sai";
+    std::cout<<names[0]<<std::endl;
+    std::cout<<names[1]<<std::endl;
+    std::cout<<names[2]<<std::endl;
+    std::cout<<names[3]<<std::endl;
+    return 0;
+}
